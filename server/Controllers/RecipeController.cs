5b156225a93787c8471678c4696999fa4c522eed
Syed Zaidi <26808641+zaidi-01@users.noreply.github.com>
2023-11-26 04:02:29 -0500

    feat: add delete recipe by id api

--- server/Controllers/RecipeController.cs
+++ server/Controllers/RecipeController.cs
@@ -1,13 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using server.Data;
-using server.Models;
 
 namespace server.Controllers
 {
   public class SearchModel
   {
-    public string SearchQuery { get; set; }
+    public string? SearchQuery { get; set; }
   }
 
   [ApiController]
@@ -28,8 +27,27 @@ namespace server.Controllers
       // TODO(StevieShibly8): Replace with stored-procedure
       var recipe = await _context.Recipe.FirstOrDefaultAsync(r => r.Id == id);
 
-      if (recipe != null) return Ok(recipe);
-      else return NotFound();
+      if (recipe == null)
+      {
+        return NotFound();
+      }
+
+      return Ok(recipe);
+    }
+
+    [HttpDelete]
+    [Route("{id:int}")]
+    public async Task<IActionResult> DeleteRecipe(int id)
+    {
+      // Delete recipe
+      var result = await _context.Recipe.Where(r => r.Id == id).ExecuteDeleteAsync();
+
+      if (result == 0)
+      {
+        return NotFound();
+      };
+
+      return Ok();
     }
 
     [HttpPost]
@@ -37,7 +55,7 @@ namespace server.Controllers
     public async Task<IActionResult> SearchRecipes([FromBody] SearchModel searchModel)
     {
       var recipes = await _context.Recipe
-        .Where(r => r.Name.ToLower().Contains(searchModel.SearchQuery.ToLower()))
+        .Where(r => r.Name.ToLower().Contains((searchModel.SearchQuery ?? "").ToLower()))
         .ToListAsync();
 
       return Ok(recipes);
