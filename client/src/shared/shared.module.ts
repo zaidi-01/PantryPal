ab7c32c1e1a5f01a458323dc446634f742d89913
Syed Zaidi <26808641+zaidi-01@users.noreply.github.com>
2023-12-03 10:19:42 -0500

    refactor: move shared components in separate dir

--- client/src/shared/shared.module.ts
+++ client/src/shared/shared.module.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { ErrorComponent } from './error/error.component';
-import { RecipeAttributeComponent } from './recipe-attribute/recipe-attribute.component';
-import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
-import { SpinnerComponent } from './spinner/spinner.component';
+import { ErrorComponent } from './components/error/error.component';
+import { RecipeAttributeComponent } from './components/recipe-attribute/recipe-attribute.component';
+import { RecipeDetailsComponent } from './components/recipe-details/recipe-details.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 @NgModule({
   declarations: [
